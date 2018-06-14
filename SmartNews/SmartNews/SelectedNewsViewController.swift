7a5bade6e424354765437b3ed37bf0485998aa93
Raul Bude <>
2018-06-14 17:13:42 +0300

    no message

---
+++ SmartNews/SmartNews/SelectedNewsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SelectedNewsViewController.swift
+//  SmartNews
+//
+//  Created by Raul Bude on 13/06/2018.
+//  Copyright © 2018 Raul Bude. All rights reserved.
+//
+
+import UIKit
+
+class SelectedNewsViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var descriptionTextView: UITextView!
+
+    @IBOutlet weak private var newsImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
